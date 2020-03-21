c219f2ebd3f4e4501f5ea9996fc91debaac0c163
Icheke Chinaza <ichekechinaza77@gmail.com>
2020-03-21 15:07:06 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = "Icheke Chinaza";
+var coursesArr = ["HTML", "CSS", "JavaScript", "Design"];
+
+console.log(name);
+console.log(coursesArr);
+
+for (let i = 1; i <= 200; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
